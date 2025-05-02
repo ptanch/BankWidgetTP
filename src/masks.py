554d52e08a7ebd8fe8072f97fd70744447063c3c
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-05-02 21:17:42 +0300

    версия без доп функционала

---
+++ src/masks.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+
+def get_mask_card_number(card_number: str) -> str:
+    """Obtaining card numbers and masking some of them"""
+
+    masked_card_number = card_number.replace(card_number[6:12], "******")
+
+    splits = []
+
+    for i in range(0, len(card_number), 4):
+        split = masked_card_number[i:i + 4]  # substring 4 symbols
+        splits.append(split)
+    splitted_card_number = " ".join(splits)
+
+    return splitted_card_number
+
+
+def get_mask_account(account_number: str) -> str:
+    """ Masking number of account"""
+
+    masked_account_number = account_number.replace(account_number[0:-4], "**")
+
+    return masked_account_number
+
+
+if __name__ == '__main__':
+
+    card_number = str(input("Введите номер карты "))
+    print(get_mask_card_number(card_number))  # 7000 79** **** 6361 function output
+    account_number = str(input("Введите номер счета "))
+    print(get_mask_account(account_number))  # **4305 function output
