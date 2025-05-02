53eb6270ba0c6cf375c15fa152a6d651bb545c04
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-05-02 22:30:25 +0300

    проверила и исправила функции линтерами

---
+++ src/widget.py
@@ -0,0 +1,33 @@
+import re
+
+from masks import get_mask_card_number, get_mask_account
+
+def mask_account_card(bank_data: str) -> str:
+    """Finding card and account numbers
+     in a string and masking them"""
+
+    def mask_account(match: str) -> str:
+        """Masking 20-digit card numbers"""
+
+        digits = match.group(1)
+        masked_account = get_mask_account(digits)
+        return f"Счет {masked_account}"
+
+    # Checking account numbers
+    bank_data = re.sub(r"Счет\s+(\d{20})", mask_account, bank_data)
+
+    def mask_card(match: str) -> str:
+        """Masking 16-digit card numbers"""
+
+        digits = match.group()
+        return get_mask_card_number(digits)
+
+    # Checking card numbers
+    bank_data = re.sub(r'\d{16}', mask_card, bank_data)
+
+    return bank_data
+
+
+if __name__ == '__main__':
+    print(mask_account_card("Счет 73654108430135874305"))
+    print(mask_account_card("Visa Platinum 7000792289606361"))
