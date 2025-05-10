2c60225950ed6e60246d22d0575e89b9327e477a
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-05-10 15:17:04 +0300

    удаление вложенных функций для упрощения проекта

--- src/widget.py
+++ src/widget.py
@@ -4,25 +4,27 @@ from re import Match
 from masks import get_mask_account, get_mask_card_number
 
 
+def mask_account(match: Match[str]) -> str:
+    """Masking 20-digit card numbers"""
+
+    digits = match.group(1)
+    masked_account = get_mask_account(digits)
+    return f"Счет {masked_account}"
+
+
+def mask_card(match: Match[str]) -> str:
+    """Masking 16-digit card numbers"""
+
+    digits = match.group()
+    return get_mask_card_number(digits)
+
+
 def mask_account_card(bank_data: str) -> str:
     """Finding card and account numbers
      in a string and masking them"""
 
-    def mask_account(match: Match[str]) -> str:
-        """Masking 20-digit card numbers"""
-
-        digits = match.group(1)
-        masked_account = get_mask_account(digits)
-        return f"Счет {masked_account}"
-
     # Checking account numbers
     bank_data = re.sub(r"Счет\s+(\d{20})", mask_account, bank_data)
-
-    def mask_card(match: Match[str]) -> str:
-        """Masking 16-digit card numbers"""
-
-        digits = match.group()
-        return get_mask_card_number(digits)
 
     # Checking card numbers
     bank_data = re.sub(r'\d{16}', mask_card, bank_data)
@@ -33,6 +35,7 @@ def mask_account_card(bank_data: str) -> str:
 if __name__ == '__main__':
     print(mask_account_card("Счет 73654108430135874305"))
     print(mask_account_card("Visa Platinum 7000792289606361"))
+    print(mask_account_card("Maestro 7000792289606361"))
 
 
 def get_date(full_date: str) -> str:
