bf2f109ca0e880e5badfa53da55075906efe6425
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-06-16 18:38:17 +0300

    отформатировано isort

--- src/count_operations.py
+++ src/count_operations.py
@@ -1,6 +1,6 @@
 import re
 from collections import Counter
-from typing import List, Dict
+from typing import Dict, List
 
 
 def process_bank_operations(data: List[Dict], categories: List[str]) -> Dict[str, int]:
